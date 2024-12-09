fbbaf00d6c05fea3e1917db50571ec783bb2604b
Milan Pjevic <milanpjevic@icloud.com>
2024-12-09 21:18:05 +0100

    The Compound Component Pattern

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ import "./index.css";
 export default function App() {
   return (
     <div>
-      <h1>Compound Component Pattern</h1>
-      <Counter
-        iconIncrease="+"
-        iconDecrease="-"
-        label="My ~NOT~ so flexible counter"
-        hideLabel={false}
-        hideIncrease={false}
-        hideDecrease={false}
-      />
+      <h1>The Compound Component Pattern</h1>
+      <Counter>
+        <Counter.Label>My super flexible counter</Counter.Label>
+        <Counter.Decrease icon="-" />
+        <Counter.Count />
+        <Counter.Increase icon="+" />
+      </Counter>
+
+      <Counter>
+        <Counter.Decrease icon="⫷" />
+        <Counter.Count />
+        <Counter.Increase icon="⫸" />
+      </Counter>
     </div>
   );
 }
